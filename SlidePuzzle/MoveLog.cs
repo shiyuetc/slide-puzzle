2cdbf4a26264d7828247e0abe8aaf3ba5676a709
shiyuetc <shiyuetc@users.noreply.github.com>
2020-01-08 16:03:29 +0900

    Add MoveLog class

---
+++ SlidePuzzle/MoveLog.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SlidePuzzle
+{
+    public class MoveLog
+    {
+        public int MoveIndex { get; set; }
+
+        public Direction MoveDirection { get; set; }
+
+        public MoveLog(int moveIndex, Direction moveDirection)
+        {
+            this.MoveIndex = moveIndex;
+            this.MoveDirection = moveDirection;
+        }
+    }
+}
