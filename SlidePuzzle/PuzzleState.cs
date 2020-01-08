addc045dbf43e3dafc86d9d384e9a00f1c2d69b3
shiyuetc <shiyuetc@users.noreply.github.com>
2020-01-08 14:57:27 +0900

    Add PuzzleState enum

---
+++ SlidePuzzle/PuzzleState.cs
@@ -0,0 +1,15 @@
+﻿
+namespace SlidePuzzle
+{
+    /// <summary>
+    /// ゲームの状態を表す列挙体
+    /// </summary>
+    public enum PuzzleState
+    {
+        WAIT, // 待機中
+        PLAYING, // プレイ中
+        STOP, // 中止
+        CLEAR, // クリア
+        SHUFFLE // シャッフル中
+    }
+}
