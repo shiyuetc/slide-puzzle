d3a77a66e60b73a410f6f0d35e31f95ff4bee45c
shiyuetc <shiyuetc@users.noreply.github.com>
2020-01-08 13:41:09 +0900

    Add GenerateForm

--- SlidePuzzle/GenerateForm.Designer.cs
+++ SlidePuzzle/GenerateForm.Designer.cs
@@ -29,18 +29,18 @@
         private void InitializeComponent()
         {
             this.InfomationGroupBox = new System.Windows.Forms.GroupBox();
+            this.BorderLabel = new System.Windows.Forms.Label();
+            this.PreviewPictureBox = new System.Windows.Forms.PictureBox();
+            this.LevelComboBox = new System.Windows.Forms.ComboBox();
+            this.ReferenceButton = new System.Windows.Forms.Button();
+            this.FilePathTextBox = new System.Windows.Forms.TextBox();
+            this.FromImageRadioButton = new System.Windows.Forms.RadioButton();
+            this.FromNumberRadioButton = new System.Windows.Forms.RadioButton();
+            this.PreviewTitleLabel = new System.Windows.Forms.Label();
+            this.LevelTitleLabel = new System.Windows.Forms.Label();
+            this.FromTitleLabel = new System.Windows.Forms.Label();
             this.GenerateButton = new System.Windows.Forms.Button();
             this.CloseButton = new System.Windows.Forms.Button();
-            this.FromTitleLabel = new System.Windows.Forms.Label();
-            this.LevelTitleLabel = new System.Windows.Forms.Label();
-            this.PreviewTitleLabel = new System.Windows.Forms.Label();
-            this.FromNumberRadioButton = new System.Windows.Forms.RadioButton();
-            this.FromImageRadioButton = new System.Windows.Forms.RadioButton();
-            this.FilePathTextBox = new System.Windows.Forms.TextBox();
-            this.ReferenceButton = new System.Windows.Forms.Button();
-            this.LevelComboBox = new System.Windows.Forms.ComboBox();
-            this.PreviewPictureBox = new System.Windows.Forms.PictureBox();
-            this.BorderLabel = new System.Windows.Forms.Label();
             this.InfomationGroupBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PreviewPictureBox)).BeginInit();
             this.SuspendLayout();
@@ -66,89 +66,22 @@
             this.InfomationGroupBox.TabStop = false;
             this.InfomationGroupBox.Text = "★ 生成情報";
             // 
-            // GenerateButton
+            // BorderLabel
             // 
-            this.GenerateButton.Location = new System.Drawing.Point(278, 248);
-            this.GenerateButton.Name = "GenerateButton";
-            this.GenerateButton.Size = new System.Drawing.Size(75, 31);
-            this.GenerateButton.TabIndex = 1;
-            this.GenerateButton.Text = "生成";
-            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.BorderLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.BorderLabel.Location = new System.Drawing.Point(251, 21);
+            this.BorderLabel.Name = "BorderLabel";
+            this.BorderLabel.Size = new System.Drawing.Size(1, 192);
+            this.BorderLabel.TabIndex = 9;
             // 
-            // CloseButton
+            // PreviewPictureBox
             // 
-            this.CloseButton.Location = new System.Drawing.Point(359, 248);
-            this.CloseButton.Name = "CloseButton";
-            this.CloseButton.Size = new System.Drawing.Size(100, 31);
-            this.CloseButton.TabIndex = 2;
-            this.CloseButton.Text = "キャンセル(&C)";
-            this.CloseButton.UseVisualStyleBackColor = true;
-            // 
-            // FromTitleLabel
-            // 
-            this.FromTitleLabel.AutoSize = true;
-            this.FromTitleLabel.Location = new System.Drawing.Point(18, 30);
-            this.FromTitleLabel.Name = "FromTitleLabel";
-            this.FromTitleLabel.Size = new System.Drawing.Size(93, 15);
-            this.FromTitleLabel.TabIndex = 0;
-            this.FromTitleLabel.Text = "♦ 生成元の画像";
-            // 
-            // LevelTitleLabel
-            // 
-            this.LevelTitleLabel.AutoSize = true;
-            this.LevelTitleLabel.Location = new System.Drawing.Point(18, 140);
-            this.LevelTitleLabel.Name = "LevelTitleLabel";
-            this.LevelTitleLabel.Size = new System.Drawing.Size(99, 15);
-            this.LevelTitleLabel.TabIndex = 1;
-            this.LevelTitleLabel.Text = "♦ パズルの難易度";
-            // 
-            // PreviewTitleLabel
-            // 
-            this.PreviewTitleLabel.AutoSize = true;
-            this.PreviewTitleLabel.Location = new System.Drawing.Point(268, 30);
-            this.PreviewTitleLabel.Name = "PreviewTitleLabel";
-            this.PreviewTitleLabel.Size = new System.Drawing.Size(67, 15);
-            this.PreviewTitleLabel.TabIndex = 2;
-            this.PreviewTitleLabel.Text = "♦ プレビュー";
-            // 
-            // FromNumberRadioButton
-            // 
-            this.FromNumberRadioButton.AutoSize = true;
-            this.FromNumberRadioButton.Location = new System.Drawing.Point(31, 57);
-            this.FromNumberRadioButton.Name = "FromNumberRadioButton";
-            this.FromNumberRadioButton.Size = new System.Drawing.Size(130, 19);
-            this.FromNumberRadioButton.TabIndex = 3;
-            this.FromNumberRadioButton.TabStop = true;
-            this.FromNumberRadioButton.Text = "番号からパズルを生成";
-            this.FromNumberRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // FromImageRadioButton
-            // 
-            this.FromImageRadioButton.AutoSize = true;
-            this.FromImageRadioButton.Location = new System.Drawing.Point(31, 82);
-            this.FromImageRadioButton.Name = "FromImageRadioButton";
-            this.FromImageRadioButton.Size = new System.Drawing.Size(164, 19);
-            this.FromImageRadioButton.TabIndex = 4;
-            this.FromImageRadioButton.TabStop = true;
-            this.FromImageRadioButton.Text = "画像ファイルからパズルを生成";
-            this.FromImageRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // FilePathTextBox
-            // 
-            this.FilePathTextBox.Font = new System.Drawing.Font("Meiryo UI", 8F);
-            this.FilePathTextBox.Location = new System.Drawing.Point(41, 107);
-            this.FilePathTextBox.Name = "FilePathTextBox";
-            this.FilePathTextBox.Size = new System.Drawing.Size(140, 21);
-            this.FilePathTextBox.TabIndex = 5;
-            // 
-            // ReferenceButton
-            // 
-            this.ReferenceButton.Location = new System.Drawing.Point(181, 106);
-            this.ReferenceButton.Name = "ReferenceButton";
-            this.ReferenceButton.Size = new System.Drawing.Size(50, 23);
-            this.ReferenceButton.TabIndex = 6;
-            this.ReferenceButton.Text = "参照";
-            this.ReferenceButton.UseVisualStyleBackColor = true;
+            this.PreviewPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PreviewPictureBox.Location = new System.Drawing.Point(277, 56);
+            this.PreviewPictureBox.Name = "PreviewPictureBox";
+            this.PreviewPictureBox.Size = new System.Drawing.Size(150, 150);
+            this.PreviewPictureBox.TabIndex = 8;
+            this.PreviewPictureBox.TabStop = false;
             // 
             // LevelComboBox
             // 
@@ -162,23 +95,98 @@
             this.LevelComboBox.Name = "LevelComboBox";
             this.LevelComboBox.Size = new System.Drawing.Size(140, 23);
             this.LevelComboBox.TabIndex = 7;
+            this.LevelComboBox.SelectedIndexChanged += new System.EventHandler(this.LevelComboBox_SelectedIndexChanged);
             // 
-            // PreviewPictureBox
+            // ReferenceButton
             // 
-            this.PreviewPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.PreviewPictureBox.Location = new System.Drawing.Point(277, 56);
-            this.PreviewPictureBox.Name = "PreviewPictureBox";
-            this.PreviewPictureBox.Size = new System.Drawing.Size(150, 150);
-            this.PreviewPictureBox.TabIndex = 8;
-            this.PreviewPictureBox.TabStop = false;
+            this.ReferenceButton.Enabled = false;
+            this.ReferenceButton.Location = new System.Drawing.Point(181, 106);
+            this.ReferenceButton.Name = "ReferenceButton";
+            this.ReferenceButton.Size = new System.Drawing.Size(50, 23);
+            this.ReferenceButton.TabIndex = 6;
+            this.ReferenceButton.Text = "参照";
+            this.ReferenceButton.UseVisualStyleBackColor = true;
+            this.ReferenceButton.Click += new System.EventHandler(this.ReferenceButton_Click);
             // 
-            // BorderLabel
+            // FilePathTextBox
             // 
-            this.BorderLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.BorderLabel.Location = new System.Drawing.Point(251, 21);
-            this.BorderLabel.Name = "BorderLabel";
-            this.BorderLabel.Size = new System.Drawing.Size(1, 192);
-            this.BorderLabel.TabIndex = 9;
+            this.FilePathTextBox.Font = new System.Drawing.Font("Meiryo UI", 8F);
+            this.FilePathTextBox.Location = new System.Drawing.Point(41, 107);
+            this.FilePathTextBox.Name = "FilePathTextBox";
+            this.FilePathTextBox.ReadOnly = true;
+            this.FilePathTextBox.Size = new System.Drawing.Size(140, 21);
+            this.FilePathTextBox.TabIndex = 5;
+            // 
+            // FromImageRadioButton
+            // 
+            this.FromImageRadioButton.AutoSize = true;
+            this.FromImageRadioButton.Location = new System.Drawing.Point(31, 82);
+            this.FromImageRadioButton.Name = "FromImageRadioButton";
+            this.FromImageRadioButton.Size = new System.Drawing.Size(164, 19);
+            this.FromImageRadioButton.TabIndex = 4;
+            this.FromImageRadioButton.TabStop = true;
+            this.FromImageRadioButton.Text = "画像ファイルからパズルを生成";
+            this.FromImageRadioButton.UseVisualStyleBackColor = true;
+            this.FromImageRadioButton.CheckedChanged += new System.EventHandler(this.FromImageRadioButton_CheckedChanged);
+            // 
+            // FromNumberRadioButton
+            // 
+            this.FromNumberRadioButton.AutoSize = true;
+            this.FromNumberRadioButton.Location = new System.Drawing.Point(31, 57);
+            this.FromNumberRadioButton.Name = "FromNumberRadioButton";
+            this.FromNumberRadioButton.Size = new System.Drawing.Size(130, 19);
+            this.FromNumberRadioButton.TabIndex = 3;
+            this.FromNumberRadioButton.TabStop = true;
+            this.FromNumberRadioButton.Text = "番号からパズルを生成";
+            this.FromNumberRadioButton.UseVisualStyleBackColor = true;
+            this.FromNumberRadioButton.CheckedChanged += new System.EventHandler(this.FromNumberRadioButton_CheckedChanged);
+            // 
+            // PreviewTitleLabel
+            // 
+            this.PreviewTitleLabel.AutoSize = true;
+            this.PreviewTitleLabel.Location = new System.Drawing.Point(268, 30);
+            this.PreviewTitleLabel.Name = "PreviewTitleLabel";
+            this.PreviewTitleLabel.Size = new System.Drawing.Size(67, 15);
+            this.PreviewTitleLabel.TabIndex = 2;
+            this.PreviewTitleLabel.Text = "♦ プレビュー";
+            // 
+            // LevelTitleLabel
+            // 
+            this.LevelTitleLabel.AutoSize = true;
+            this.LevelTitleLabel.Location = new System.Drawing.Point(18, 140);
+            this.LevelTitleLabel.Name = "LevelTitleLabel";
+            this.LevelTitleLabel.Size = new System.Drawing.Size(99, 15);
+            this.LevelTitleLabel.TabIndex = 1;
+            this.LevelTitleLabel.Text = "♦ パズルの難易度";
+            // 
+            // FromTitleLabel
+            // 
+            this.FromTitleLabel.AutoSize = true;
+            this.FromTitleLabel.Location = new System.Drawing.Point(18, 30);
+            this.FromTitleLabel.Name = "FromTitleLabel";
+            this.FromTitleLabel.Size = new System.Drawing.Size(93, 15);
+            this.FromTitleLabel.TabIndex = 0;
+            this.FromTitleLabel.Text = "♦ 生成元の画像";
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(278, 248);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(75, 31);
+            this.GenerateButton.TabIndex = 1;
+            this.GenerateButton.Text = "生成(&O)";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(359, 248);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(100, 31);
+            this.CloseButton.TabIndex = 2;
+            this.CloseButton.Text = "キャンセル(&C)";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
             // 
             // GenerateForm
             // 
@@ -195,6 +203,7 @@
             this.Name = "GenerateForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "新規パズル生成";
+            this.Load += new System.EventHandler(this.GenerateForm_Load);
             this.InfomationGroupBox.ResumeLayout(false);
             this.InfomationGroupBox.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PreviewPictureBox)).EndInit();
--- SlidePuzzle/GenerateForm.cs
+++ SlidePuzzle/GenerateForm.cs
@@ -1,20 +1,198 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SlidePuzzle
 {
+    /// <summary>
+    /// パズル情報を生成するフォーム
+    /// </summary>
     public partial class GenerateForm : Form
     {
+        /// <summary>
+        /// 最終的に呼び出し元に渡すパズル情報
+        /// </summary>
+        public Puzzle MyPuzzle { get; set; }
+
+        /// <summary>
+        /// パズル生成に使用する画像
+        /// </summary>
+        private Image PuzzleImage { get; set; }
+
+        /// <summary>
+        /// フォームのコンストラクタ
+        /// </summary>
         public GenerateForm()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 埋め込みリソースに指定されている画像を取得する
+        /// </summary>
+        /// <param name="path">取得するファイル名</param>
+        /// <returns>取得した埋め込みリソース</returns>
+        private Image GetResourceImage(string path)
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            return new Bitmap(assembly.GetManifestResourceStream("SlidePuzzle.Images." + path));
+        }
+
+        /// <summary>
+        /// 選択した画像ファイルを加工して取得する
+        /// </summary>
+        /// <returns>加工済みの画像</returns>
+        private Image LoadImage()
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog
+                {
+                    Filter = "イメージファイル(*.jpg;*.jpeg;*.png)|*.jpg;*.jpeg;*.png|すべてのファイル(*.*)|*.*",
+                    Title = "開く画像ファイルを選択してください",
+                    RestoreDirectory = true
+                };
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    System.IO.FileStream fs = new System.IO.FileStream(
+                        ofd.FileName,
+                        System.IO.FileMode.Open,
+                        System.IO.FileAccess.Read
+                    );
+                    Image image = Image.FromStream(fs);
+                    fs.Close();
+
+                    if (image.Width < 300 || image.Height < 300)
+                    {
+                        MessageBox.Show("サイズが縦" + 300 + "px、横" + 300 + "px以上の画像にして！", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        // スキャン用のフォームを表示
+                        this.FilePathTextBox.Text = ofd.FileName;
+                        ImageScannerForm form = new ImageScannerForm(image, this.LevelComboBox.SelectedIndex + 1);
+                        form.Left = this.Left + (this.Width - form.Width) / 2;
+                        form.Top = this.Top + (this.Height - form.Height) / 2;
+                        form.ShowDialog();
+
+                        // 確定された画像があれば返す
+                        if(form.TrimedImage != null)
+                            return form.TrimedImage.Resize(300);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message == "使用されたパラメーターが有効ではありません。")
+                {
+                    MessageBox.Show("選択したファイルは対応してません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            return null;
+        }
+        
+        /// <summary>
+        /// プレビュー画像の更新を行う
+        /// </summary>
+        private void UpdatePreview()
+        {
+            if (this.PuzzleImage != null) this.PreviewPictureBox.Image = this.PuzzleImage.Resize(150).DrawMeshLine(this.LevelComboBox.SelectedIndex + 2);
+        }
+        
+        /// <summary>
+        /// フォームのロードイベント
+        /// </summary>
+        private void GenerateForm_Load(object sender, EventArgs e)
+        {
+            // パズルの難易度Lv1を選択
+            LevelComboBox.SelectedIndex = 0;
+        }
+        
+        /// <summary>
+        /// パズル生成元ラジオボタン(番号)の値変更イベント
+        /// </summary>
+        private void FromNumberRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.FromNumberRadioButton.Checked)
+            {
+                this.PuzzleImage = this.GetResourceImage("level" + (this.LevelComboBox.SelectedIndex + 1) + ".PNG");
+                this.FilePathTextBox.Clear();
+                this.ReferenceButton.Enabled = false;
+                this.UpdatePreview();
+            }
+        }
+        
+        /// <summary>
+        /// パズル生成元ラジオボタン(画像)の値変更イベント
+        /// </summary>
+        private void FromImageRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.FromImageRadioButton.Checked)
+            {
+                Image openImage = this.LoadImage();
+                if (openImage == null)
+                {
+                    this.FilePathTextBox.Clear();
+                    this.FromNumberRadioButton.Checked = true;
+                }
+                else
+                {
+                    this.PuzzleImage = openImage;
+                    this.PreviewPictureBox.Image = ImageTransformation.Resize(this.PuzzleImage, 150).DrawMeshLine(this.LevelComboBox.SelectedIndex + 1);
+                    this.ReferenceButton.Enabled = true;
+                    this.UpdatePreview();
+                }
+            }
+        }
+        
+        /// <summary>
+        /// 参照ボタンのクリックイベント
+        /// </summary>
+        private void ReferenceButton_Click(object sender, EventArgs e)
+        {
+            Image openImage = this.LoadImage();
+            if (openImage != null)
+            {
+                this.PuzzleImage = openImage;
+                this.PreviewPictureBox.Image = ImageTransformation.Resize(this.PuzzleImage, 150).DrawMeshLine(this.LevelComboBox.SelectedIndex + 1);
+                this.UpdatePreview();
+            }
+        }
+        
+        /// <summary>
+        /// パズル難易度の値変更イベント
+        /// </summary>
+        private void LevelComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // 番号からパズルを生成を選択している場合に画像も入れ替える
+            if (this.FromNumberRadioButton.Checked)
+            {
+                this.PuzzleImage = this.GetResourceImage("level" + (this.LevelComboBox.SelectedIndex + 1) + ".PNG");
+            }
+
+            this.UpdatePreview();
+        }
+        
+        /// <summary>
+        /// 生成ボタンのクリックイベント
+        /// </summary>
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            this.MyPuzzle = new Puzzle(this.PuzzleImage, this.LevelComboBox.SelectedIndex + 1);
+            this.Close();
+        }
+        
+        /// <summary>
+        /// キャンセルボタンのクリックイベント
+        /// </summary>
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
