b660ef1a0fed9776ba84c8c8fffda1c22076c843
shiyuetc <shiyuetc@users.noreply.github.com>
2020-01-08 16:38:12 +0900

    Add PlayForm

--- SlidePuzzle/PlayForm.Designer.cs
+++ SlidePuzzle/PlayForm.Designer.cs
@@ -28,18 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.GenerateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.BoardPannel = new System.Windows.Forms.Panel();
+            this.BoardPanel = new System.Windows.Forms.Panel();
             this.AssistLabel = new System.Windows.Forms.Label();
-            this.StatusTitleLabel = new System.Windows.Forms.Label();
-            this.StatusLabel = new System.Windows.Forms.Label();
+            this.StateTitleLabel = new System.Windows.Forms.Label();
+            this.StateLabel = new System.Windows.Forms.Label();
             this.LevelTitleLabel = new System.Windows.Forms.Label();
             this.LevelLabel = new System.Windows.Forms.Label();
             this.TimeTitleLabel = new System.Windows.Forms.Label();
             this.TimeLabel = new System.Windows.Forms.Label();
             this.CountTitleLabel = new System.Windows.Forms.Label();
-            this.CountLabel = new System.Windows.Forms.Label();
+            this.SlideCountLabel = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,15 +61,16 @@
             this.GenerateToolStripMenuItem.Name = "GenerateToolStripMenuItem";
             this.GenerateToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
             this.GenerateToolStripMenuItem.Text = "新規パズル生成";
+            this.GenerateToolStripMenuItem.Click += new System.EventHandler(this.GenerateToolStripMenuItem_Click);
             // 
-            // BoardPannel
+            // BoardPanel
             // 
-            this.BoardPannel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.BoardPannel.Location = new System.Drawing.Point(12, 37);
-            this.BoardPannel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.BoardPannel.Name = "BoardPannel";
-            this.BoardPannel.Size = new System.Drawing.Size(300, 300);
-            this.BoardPannel.TabIndex = 1;
+            this.BoardPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BoardPanel.Location = new System.Drawing.Point(12, 37);
+            this.BoardPanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.BoardPanel.Name = "BoardPanel";
+            this.BoardPanel.Size = new System.Drawing.Size(300, 300);
+            this.BoardPanel.TabIndex = 1;
             // 
             // AssistLabel
             // 
@@ -79,27 +82,27 @@
             this.AssistLabel.TabIndex = 21;
             this.AssistLabel.Text = "※W,A,S,Dキー、矢印キーまたは左クリックでパネルを移動";
             // 
-            // StatusTitleLabel
+            // StateTitleLabel
             // 
-            this.StatusTitleLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.StatusTitleLabel.Location = new System.Drawing.Point(318, 37);
-            this.StatusTitleLabel.Name = "StatusTitleLabel";
-            this.StatusTitleLabel.Size = new System.Drawing.Size(117, 25);
-            this.StatusTitleLabel.TabIndex = 22;
-            this.StatusTitleLabel.Text = "状態";
-            this.StatusTitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.StateTitleLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.StateTitleLabel.Location = new System.Drawing.Point(318, 37);
+            this.StateTitleLabel.Name = "StateTitleLabel";
+            this.StateTitleLabel.Size = new System.Drawing.Size(117, 25);
+            this.StateTitleLabel.TabIndex = 22;
+            this.StateTitleLabel.Text = "状態";
+            this.StateTitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // StatusLabel
+            // StateLabel
             // 
-            this.StatusLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.StatusLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.StatusLabel.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.StatusLabel.Location = new System.Drawing.Point(318, 62);
-            this.StatusLabel.Name = "StatusLabel";
-            this.StatusLabel.Size = new System.Drawing.Size(117, 43);
-            this.StatusLabel.TabIndex = 23;
-            this.StatusLabel.Text = "待機中";
-            this.StatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.StateLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.StateLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.StateLabel.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.StateLabel.Location = new System.Drawing.Point(318, 62);
+            this.StateLabel.Name = "StateLabel";
+            this.StateLabel.Size = new System.Drawing.Size(117, 43);
+            this.StateLabel.TabIndex = 23;
+            this.StateLabel.Text = "待機中";
+            this.StateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // LevelTitleLabel
             // 
@@ -155,33 +158,38 @@
             this.CountTitleLabel.Text = "回転回数";
             this.CountTitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // CountLabel
+            // SlideCountLabel
             // 
-            this.CountLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.CountLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.CountLabel.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.CountLabel.Location = new System.Drawing.Point(318, 275);
-            this.CountLabel.Name = "CountLabel";
-            this.CountLabel.Size = new System.Drawing.Size(117, 43);
-            this.CountLabel.TabIndex = 29;
-            this.CountLabel.Text = "0回";
-            this.CountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.SlideCountLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.SlideCountLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.SlideCountLabel.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.SlideCountLabel.Location = new System.Drawing.Point(318, 275);
+            this.SlideCountLabel.Name = "SlideCountLabel";
+            this.SlideCountLabel.Size = new System.Drawing.Size(117, 43);
+            this.SlideCountLabel.TabIndex = 29;
+            this.SlideCountLabel.Text = "0回";
+            this.SlideCountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Interval = 1000;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
             // 
             // PlayForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(447, 363);
-            this.Controls.Add(this.CountLabel);
+            this.Controls.Add(this.SlideCountLabel);
             this.Controls.Add(this.CountTitleLabel);
             this.Controls.Add(this.TimeLabel);
             this.Controls.Add(this.TimeTitleLabel);
             this.Controls.Add(this.LevelLabel);
             this.Controls.Add(this.LevelTitleLabel);
-            this.Controls.Add(this.StatusLabel);
-            this.Controls.Add(this.StatusTitleLabel);
+            this.Controls.Add(this.StateLabel);
+            this.Controls.Add(this.StateTitleLabel);
             this.Controls.Add(this.AssistLabel);
-            this.Controls.Add(this.BoardPannel);
+            this.Controls.Add(this.BoardPanel);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -190,6 +198,7 @@
             this.MaximizeBox = false;
             this.Name = "PlayForm";
             this.Text = "SlidePuzzle";
+            this.Load += new System.EventHandler(this.PlayForm_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -201,16 +210,17 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem GenerateToolStripMenuItem;
-        private System.Windows.Forms.Panel BoardPannel;
+        private System.Windows.Forms.Panel BoardPanel;
         private System.Windows.Forms.Label AssistLabel;
-        private System.Windows.Forms.Label StatusTitleLabel;
-        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Label StateTitleLabel;
+        private System.Windows.Forms.Label StateLabel;
         private System.Windows.Forms.Label LevelTitleLabel;
         private System.Windows.Forms.Label LevelLabel;
         private System.Windows.Forms.Label TimeTitleLabel;
         private System.Windows.Forms.Label TimeLabel;
         private System.Windows.Forms.Label CountTitleLabel;
-        private System.Windows.Forms.Label CountLabel;
+        private System.Windows.Forms.Label SlideCountLabel;
+        private System.Windows.Forms.Timer GameTimer;
     }
 }
 
--- SlidePuzzle/PlayForm.cs
+++ SlidePuzzle/PlayForm.cs
@@ -1,20 +1,300 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SlidePuzzle
 {
+    /// <summary>
+    /// メインのフォーム
+    /// </summary>
     public partial class PlayForm : Form
     {
+        /// <summary>
+        /// ゲームを制御するパズル情報
+        /// </summary>
+        private Puzzle MainPuzzle { get; set; }
+
+        /// <summary>
+        /// 盤面に対応した画像UIの配列
+        /// </summary>
+        private PictureBox[] BoardPictures { get; set; }
+
+        /// <summary>
+        /// ゲームの状態を表す
+        /// </summary>
+        private PuzzleState State { get; set; } = PuzzleState.WAIT;
+
+        /// <summary>
+        /// ゲーム状態に対応した色のリスト
+        /// </summary>
+        private Dictionary<PuzzleState, Color[]> StateColors { get; set; }
+
+        private int ProgressTime { get; set; }
+
+        /// <summary>
+        /// 移動判定フラグ用の変数
+        /// </summary>
+        private bool PictureMoving { get; set; }
+
+        /// <summary>
+        /// フォームのコンストラクタ
+        /// </summary>
         public PlayForm()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// フォームのロードイベント
+        /// </summary>
+        private void PlayForm_Load(object sender, EventArgs e)
+        {
+            // 色情報を格納する
+            this.StateColors = new Dictionary<PuzzleState, Color[]>();
+            this.StateColors.Add(PuzzleState.WAIT, new Color[] {
+                ColorTranslator.FromHtml("0x696969"),
+                ColorTranslator.FromHtml("0xdcdcdc")});
+            this.StateColors.Add(PuzzleState.PLAYING, new Color[] {
+                ColorTranslator.FromHtml("0x804000"),
+                ColorTranslator.FromHtml("0xffc080")});
+            this.StateColors.Add(PuzzleState.STOP, new Color[] {
+                ColorTranslator.FromHtml("0x808000"),
+                ColorTranslator.FromHtml("0xffff80")});
+            this.StateColors.Add(PuzzleState.CLEAR, new Color[] {
+                ColorTranslator.FromHtml("0x008000"),
+                ColorTranslator.FromHtml("0x80ff80")});
+            this.StateColors.Add(PuzzleState.SHUFFLE, new Color[] {
+                ColorTranslator.FromHtml("0x800080"),
+                ColorTranslator.FromHtml("0xff80ff")});
+        }
+
+        /// <summary>
+        /// ゲームの状態を更新する
+        /// </summary>
+        /// <param name="state">新しく更新する状態</param>
+        private void SetState(PuzzleState state)
+        {
+            switch (state)
+            {
+                case PuzzleState.WAIT:
+                    this.GameTimer.Stop();
+                    this.StateLabel.Text = "待機中";
+                    break;
+                case PuzzleState.PLAYING:
+                    this.GameTimer.Start();
+                    this.StateLabel.Text = "ゲームプレイ中";
+                    break;
+                case PuzzleState.STOP:
+                    this.GameTimer.Stop();
+                    this.StateLabel.Text = "一時停止中";
+                    break;
+                case PuzzleState.CLEAR:
+                    this.GameTimer.Stop();
+                    this.StateLabel.Text = "ゲームクリア";
+                    break;
+                case PuzzleState.SHUFFLE:
+                    this.StateLabel.Text = "シャッフル中";
+                    break;
+            }
+            this.StateLabel.ForeColor = this.StateColors[state][0];
+            this.StateLabel.BackColor = this.StateColors[state][1];
+            this.State = state;
+        }
+
+        /// <summary>
+        /// パズル新規生成ボタンのクリックイベント
+        /// </summary>
+        private void GenerateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // シャッフル中の場合はそのままリターンする
+            if (this.State == PuzzleState.SHUFFLE) return;
+
+            // ゲームプレイ中の場合は一時停止にする
+            if (this.State == PuzzleState.PLAYING)
+            {
+                this.SetState(PuzzleState.STOP);
+            }
+
+            // パズルの生成画面を表示
+            GenerateForm f = new GenerateForm();
+            f.Left = this.Left + (this.Width - f.Width) / 2;
+            f.Top = this.Top + (this.Height - f.Height) / 2;
+            f.ShowDialog();
+
+            // パズル未作成の場合はそのまま終了
+            if (f.MyPuzzle == null)
+            {
+                // 一時停止の場合はゲームプレイ中に戻す
+                if (this.State == PuzzleState.STOP)
+                {
+                    this.SetState(PuzzleState.PLAYING);
+                }
+                return;
+            }
+
+            // 初期化
+            this.SetState(PuzzleState.WAIT);
+            this.MainPuzzle = f.MyPuzzle;
+            this.LevelLabel.Text = "Lv" + this.MainPuzzle.Level;
+            this.ProgressTime = 0;
+            this.TimeLabel.Text = "0秒";
+            this.SlideCountLabel.Text = "0回";
+
+            // パズルを画面に生成
+            this.BoardPictures = new PictureBox[this.MainPuzzle.MassCount];
+            for (int i = 0; i < this.MainPuzzle.MassCount; i++)
+            {
+                this.BoardPictures[i] = new PictureBox
+                {
+                    Size = new Size(this.MainPuzzle.MassWidth, this.MainPuzzle.MassWidth),
+                    Location = new Point(i % this.MainPuzzle.SplitCount * this.MainPuzzle.MassWidth, i / this.MainPuzzle.SplitCount * this.MainPuzzle.MassWidth),
+                    Image = this.MainPuzzle.MassImage[i],
+                    BackColor = Color.White
+                };
+                this.BoardPictures[i].Click += new EventHandler(BoardPictureBox_Click);
+            }
+            this.BoardPictures[this.MainPuzzle.MassCount - 1].Visible = false;
+            this.BoardPanel.Controls.Clear();
+            this.BoardPanel.Controls.AddRange(this.BoardPictures);
+
+            // 盤面のシャッフルを行う
+            this.Shuffle(new Random().Next(45, 55) * this.MainPuzzle.Level);
+        }
+
+        /// <summary>
+        /// ボード上のピクチャボックスのクリックイベント
+        /// </summary>
+        private void BoardPictureBox_Click(object sender, EventArgs e)
+        {
+            // クリックしたピクチャボックスの座標から番号を特定
+            PictureBox picture = (PictureBox)sender;
+            int index = (picture.Location.X / this.MainPuzzle.MassWidth) +
+                (picture.Location.Y / this.MainPuzzle.MassWidth) * this.MainPuzzle.SplitCount;
+            this.MovePicture(index);
+        }
+        
+        /// <summary>
+        /// 経過時間を計測する用のタイマイベント
+        /// </summary>
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            this.TimeLabel.Text = ++this.ProgressTime + "秒";
+        }
+
+        /// <summary>
+        /// 指定した回数分パズルをシャッフルする
+        /// </summary>
+        /// <param name="count">シャッフルする回数</param>
+        private async void Shuffle(int count)
+        {
+            this.SetState(PuzzleState.SHUFFLE);
+            for (int i = 0; i < count; i++)
+            {
+                this.RandomMovePicture();
+                await Task.Delay(30);
+            }
+            this.SetState(PuzzleState.PLAYING);
+        }
+        
+        /// <summary>
+        /// ランダムなマスを移動(移動モーション無し)
+        /// </summary>
+        /// <param name="index">移動するマスのインデックス</param>
+        private void RandomMovePicture()
+        {
+            // 移動判定フラグが真なら終了
+            if (this.PictureMoving) return;
+            this.PictureMoving = true;
+            
+            // パズルを回転
+            MoveLog moveLog = this.MainPuzzle.RandomMove();
+
+            // 移動に成功したら画面の画像を同期
+            if (moveLog.MoveDirection != Direction.None)
+            {
+                // 方角を計算して画像を動かす
+                int topLocation = this.BoardPictures[moveLog.MoveIndex].Location.Y;
+                int leftLocation = this.BoardPictures[moveLog.MoveIndex].Location.X;
+
+                if (moveLog.MoveDirection == Direction.W)
+                    topLocation -= this.MainPuzzle.MassWidth;
+                else if (moveLog.MoveDirection == Direction.S)
+                    topLocation += this.MainPuzzle.MassWidth;
+
+                if (moveLog.MoveDirection == Direction.A)
+                    leftLocation -= this.MainPuzzle.MassWidth;
+                else if (moveLog.MoveDirection == Direction.D)
+                    leftLocation += this.MainPuzzle.MassWidth;
+
+                this.BoardPictures[moveLog.MoveIndex].Location = new Point(leftLocation, topLocation);
+            }
+
+            // 移動判定フラグを偽にする
+            this.PictureMoving = false;
+        }
+
+        /// <summary>
+        /// 指定したマスのインデックスが移動可能なら移動(移動モーション有り)
+        /// </summary>
+        /// <param name="index">移動するマスのインデックス</param>
+        private async void MovePicture(int index)
+        {
+            // 移動判定フラグが真なら終了
+            if (this.PictureMoving || this.State != PuzzleState.PLAYING) return;
+            this.PictureMoving = true;
+            
+            // パズルを回転
+            MoveLog moveLog = this.MainPuzzle.Move(index);
+
+            // 移動に成功したら画面の画像を同期
+            if (moveLog.MoveDirection != Direction.None)
+            {
+                // 方角を計算
+                int topDirection = 0, leftDirection = 0;
+
+                if (moveLog.MoveDirection == Direction.W)
+                    topDirection = -1;
+                else if (moveLog.MoveDirection == Direction.S)
+                    topDirection = 1;
+
+                if (moveLog.MoveDirection == Direction.A)
+                    leftDirection = -1;
+                else if (moveLog.MoveDirection == Direction.D)
+                    leftDirection = 1;
+
+                // 滑らかに移動
+                for (int i = 0; i < this.MainPuzzle.MassWidth / 2; i++)
+                {
+                    this.BoardPictures[moveLog.MoveIndex].Top += 2 * topDirection;
+                    this.BoardPictures[moveLog.MoveIndex].Left += 2 * leftDirection;
+                    await Task.Run(() => System.Threading.Thread.Sleep(1));
+                }
+
+                // 余りが出た場合の調整
+                if (this.MainPuzzle.MassWidth % 2 == 1)
+                {
+                    this.BoardPictures[moveLog.MoveIndex].Top += 1 * topDirection;
+                    this.BoardPictures[moveLog.MoveIndex].Left += 1 * leftDirection;
+                }
+
+                // スライド回数をラベルに反映
+                this.SlideCountLabel.Text = this.MainPuzzle.SlideCount + "回";
+
+                // ゲームクリア判定
+                if (this.MainPuzzle.ClearCheck())
+                {
+                    this.SetState(PuzzleState.CLEAR);
+                    this.BoardPictures[this.MainPuzzle.MassCount - 1].Visible = true;
+                    MessageBox.Show("経過時間：" + this.ProgressTime +
+                        "秒\r\nスライド回数：" + this.MainPuzzle.SlideCount + "回",
+                        "ゲームクリア", MessageBoxButtons.OK);
+                }
+            }
+
+            // 移動判定フラグを偽にする
+            this.PictureMoving = false;
         }
     }
 }
